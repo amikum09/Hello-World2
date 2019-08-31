46087f05c1f52bd9ad426ab649723b3bcc184374
amikum09 <amitx04@gmail.com>
2019-08-31 11:48:11 +0530

    Second commit

--- src/com/in/TestHello.java
+++ src/com/in/TestHello.java
@@ -5,11 +5,20 @@ import java.util.*;
 public class TestHello {
 
 	public static void main(String[] args) {
+		//forEach example
+		
 		List<String> l=new ArrayList<String>();
 		l.add("amit");
 		l.add("sumit");
 		l.add("vineet");
 		l.forEach(x -> System.out.println(x));
+		
+		//stream example
+		
+		int i=(int) l.stream()
+		.filter(y -> y.equalsIgnoreCase("sumit"))
+		.count();
+		System.out.println(i);
 	}
 
 }
